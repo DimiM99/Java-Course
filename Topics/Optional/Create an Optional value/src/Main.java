e8007d360faffcd1d04ba1b4c7bc28a9409a777a
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2022-06-19 01:57:15 +0200

    ongoing with optional

---
+++ Topics/Optional/Create an Optional value/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Optional;
+import java.util.Scanner;
+
+
+class Main {
+    public static void main(String[] args) {
+        InputStringReader reader = new InputStringReader();        
+        Optional<String> value = reader.getValue();
+        if (value.isPresent()) {
+            System.out.println("Value is present: " + value.get());
+        } else {
+            System.out.println("Value is empty");
+        }
+    }
+}
+
+class InputStringReader {
+    public Optional<String> getValue() {
+        Scanner scanner = new Scanner(System.in);
+        String val = scanner.next();
+        Optional<String> result;
+
+        if ("empty".equals(val)) {
+            result = Optional.empty();
+        } else  {
+            result = Optional.of(val);
+        }
+        return  result;
+
+    }
+}
